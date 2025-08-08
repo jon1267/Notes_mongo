0f2ad3f7aae4a4c131b674c0676a44b33a37fed3
jon1267 <jonleev@gmail.com>
2025-08-08 09:00:52 +0300

    frontend react

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,23 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
 
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// this middleware 
-// will parse JSON body: {title, content} = req.body;
-app.use(express.json());
+//cors middleware
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 
+// this middleware will parse JSON body: {title, content} = req.body;
+app.use(express.json());
 // from time 1:33:00 upstash.com to time 1:45:00 create rateLimiter
 // app.use(rateLimiter);
-
-// stop at time 1:46:30
 
 app.use('/api/notes', notesRoutes);
 
