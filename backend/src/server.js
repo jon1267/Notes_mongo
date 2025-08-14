1267785358ec94aa749dae1a42c622ef7976b6d7
jon1267 <jonleev@gmail.com>
2025-08-14 18:25:40 +0300

    changes for production, but all work in dev

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 
 dotenv.config()
+
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //cors middleware
-app.use(cors({
-  origin: 'http://localhost:5173',
-}));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
+    origin: 'http://localhost:5173',
+  }));
+}
 
 // this middleware will parse JSON body: {title, content} = req.body;
 app.use(express.json());
@@ -20,6 +25,14 @@ app.use(express.json());
 // app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname,'../frontend','dist', 'index.html'));
+  });
+}
 
 connectDB().then( () => {
   app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// localhost для development режима. для production not
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 const api = axios.create({
   baseURL : 'http://localhost:5001/api' 
 });
